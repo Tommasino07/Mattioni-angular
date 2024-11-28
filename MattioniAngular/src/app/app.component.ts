de6e0ac5e06484f024679a02315b78f7f77fc000
Tommaso Mattioni <mattionitommaso09@gmail.com>
2024-11-28 09:07:47 +0000

    MattioniAngular

---
+++ MattioniAngular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, SecondComponent,ThirdComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'MattioniAngular';
+}
